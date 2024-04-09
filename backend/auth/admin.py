9df6814dd81ee1c8f1f5438b35d53e23aeb95a31
OhJongMin <jmoh7480@gmail.com>
2024-04-09 17:00:17 +0900

    feat(auth/admin): add user admin

--- backend/auth/admin.py
+++ backend/auth/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = [
+        (None, {"fields": ("username", "password")}),
+        ("정보", {"fields": ("first_name", "last_name", "email", "phone_number", "birth")}),
+        ("권한", {"fields": ("is_active", "is_staff", "is_superuser")}),
+        ("일정", {"fields": ("last_login", "date_joined")}),
+    ]
