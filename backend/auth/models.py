41f4b7b7d0e8d008cf1303d97b9a77d36ba79013
OhJongMin <jmoh7480@gmail.com>
2024-04-09 16:40:51 +0900

    feat(auth/models): add User model

--- backend/auth/models.py
+++ backend/auth/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    is_senior = models.BooleanField(default=False)
+    phone_number = models.CharField("핸드폰번호", max_length=20, null=True)
+    birth = models.DateField("생년월일", null=True)
