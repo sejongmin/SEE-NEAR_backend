fc67f5cdde3aee8450424515c601e8f52fb5a840
OhJongMin <jmoh7480@gmail.com>
2024-04-09 16:43:43 +0900

    feat(auth/views): add signup

--- backend/auth/views.py
+++ backend/auth/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate
+from rest_framework import status
+from rest_framework.decorators import api_view, authentication_classes, permission_classes, action
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from .models import User
+from .serializers import UserSerializer
 
-# Create your views here.
+@api_view(['POST'])
+def signup(request):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        user = User.objects.get(username=request.data['username'])
+        token = Token.objects.get(user=user)
+        data = {
+            "user": serializer.data,
+            "token": token.key
+        }
+        return Response(data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
