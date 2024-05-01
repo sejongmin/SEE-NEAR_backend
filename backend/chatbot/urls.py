c4a544e8a6c8e3a6501273d87158f9183de94bc1
JaeSeung-Park <wotmd36963@gmail.com>
2024-05-01 14:11:24 +0900

    feat(chatbot): chatbot

--- backend/chatbot/urls.py
+++ backend/chatbot/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('chatbot/', views.chatbot, name='voice-chatbot'),
     path('endConversation/', views.endConversation, name='endConversation'),
     path('startConversation/', views.startConversation, name='startConversation'),
-    path('getSummary/', views.getSummary, name='getSummary'),
+    # path('getSummary/', views.getSummary, name='getSummary'),
 ]
